d4c185ae3c4e338a147c181b3ca3126a52ff2a53
coderaptor12 <vaishnavinandpatel@gmail.com>
2026-03-01 15:32:34 +0530

    Add files via upload
    upload backend

--- app.py
+++ app.py
@@ -189,4 +189,8 @@ def trackorder():
 
 # RUN SERVER
 
-app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
